4a793a7eb3ceaddece191ca98d90fcc63032ec84
BeoDVB <127584129+tinwinvn@users.noreply.github.com>
2026-04-22 13:19:14 +0700

    [Feat] Education & Collaboration

--- src/common/Body/BodyGrid.tsx
+++ src/common/Body/BodyGrid.tsx
@@ -2,11 +2,17 @@ import { NewsSection } from "./NewsSection/NewsSection";
 import { Achievements } from "./Achievements/Achievements";
 import { QuickLinks } from "./QuickLinks/QuickLinks";
 import { CampusCarousel } from "./CampusCarousel/CampusCarousel";
+import { Admissions } from "./Admissions/Admissions";
+import { Education } from "./Education/Education";
+import { Collaboration } from "./Collaboration/Collaboration";
 
 export const BodyGrid = () => {
   return (
     <main>
       <NewsSection />
+      <Admissions />
+      <Education />
+      <Collaboration />
       <Achievements />
       <QuickLinks />
       <CampusCarousel />
